61215f57fc8d8d06fdb1db4b4b16348a43bd9421
Marcelle Oberholzer <marcelleober@gmail.com>
2026-08-18 21:11:07 +0200

    initial start with app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 dark:bg-black">
+      <h1 className="text-5xl font-semibold text-black dark:text-zinc-50">
+        Sittr
+      </h1>
+    </div>
+  );
+}
